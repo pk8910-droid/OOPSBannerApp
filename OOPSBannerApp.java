a994e5451f528dfb957c85da05c9e0e05fdf9762
Pravin <pk8910@srmist.edu.in>
2026-02-18 15:47:20 +0530

    UC5:Array init

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,17 +1,20 @@
-public class UC4PrintBanner {
+public class OOPSBannerApp {
 
     public static void main(String[] args) {
-        String[] lines = new String[6];
 
-        // OOPS Banner using array (UC4)
-        lines[0] = String.join(" ", "  ***  ", "  ***  ", " ****  ", " ***** ");
-        lines[1] = String.join(" ", " *   * ", " *   * ", " *   * ", " *     ");
-        lines[2] = String.join(" ", "*     *", "*     *", " ****  ", " ****  ");
-        lines[3] = String.join(" ", "*     *", "*     *", " *     ", "     * ");
-        lines[4] = String.join(" ", " *   * ", " *   * ", " *     ", " *   * ");
-        lines[5] = String.join(" ", "  ***  ", "  ***  ", " *     ", " ***** ");
+        String[] bannerLines = {
+            String.join(" ", "   ***", "       ***", "     ******", "       *****"),
+            String.join(" ", " **   **", "   **   **", "   **    **", "    **"),
+            String.join(" ", "**     **", " **     **", "  **     **", "  **"),
+            String.join(" ", "**     **", " **     **", "  **    **", "    **"),
+            String.join(" ", "**     **", " **     **", "  ******", "       ***"),
+            String.join(" ", "**     **", " **     **", "  **", "             **"),
+            String.join(" ", "**     **", " **     **", "  **", "              **"),
+            String.join(" ", " **   **", "   **   **", "  **", "             **"),
+            String.join(" ", "   ***", "       ***", "     **", "        *****")
+        };
 
-        for (String line : lines) {
+        for (String line : bannerLines) {
             System.out.println(line);
         }
     }
