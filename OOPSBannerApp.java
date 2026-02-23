1e22bb0dfd789328118f4251d25467cbdef70bd2
Pravin <pk8910@srmist.edu.in>
2026-02-23 09:10:25 +0530

    UC6-Static Functions

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,21 +1,51 @@
 public class OOPSBannerApp {
+   public static String[] getOPattern() {
+        return new String[]{
+                " ***** ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                " ***** "
+        };
+    }
+
+    public static String[] getPPattern() {
+        return new String[]{
+                "****** ",
+                "*     *",
+                "*     *",
+                "****** ",
+                "*      ",
+                "*      ",
+                "*      "
+        };
+    }
+
+    public static String[] getSPattern() {
+        return new String[]{
+                " ***** ",
+                "*      ",
+                "*      ",
+                " ***** ",
+                "      *",
+                "      *",
+                " ***** "
+        };
+    }
 
     public static void main(String[] args) {
 
-        String[] bannerLines = {
-            String.join(" ", "   ***", "       ***", "     ******", "       *****"),
-            String.join(" ", " **   **", "   **   **", "   **    **", "    **"),
-            String.join(" ", "**     **", " **     **", "  **     **", "  **"),
-            String.join(" ", "**     **", " **     **", "  **    **", "    **"),
-            String.join(" ", "**     **", " **     **", "  ******", "       ***"),
-            String.join(" ", "**     **", " **     **", "  **", "             **"),
-            String.join(" ", "**     **", " **     **", "  **", "              **"),
-            String.join(" ", " **   **", "   **   **", "  **", "             **"),
-            String.join(" ", "   ***", "       ***", "     **", "        *****")
-        };
+        String[] o = getOPattern();
+        String[] p = getPPattern();
+        String[] s = getSPattern();
 
-        for (String line : bannerLines) {
-            System.out.println(line);
+        for (int i = 0; i < o.length; i++) {
+            System.out.println(o[i] + "  " +
+                               o[i] + "  " +
+                               p[i] + "  " +
+                               s[i]);
         }
     }
 }
